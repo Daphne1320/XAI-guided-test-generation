5d62339f3359471cc4d2e8a53fcf49ef98753675
Shangsu Feng <shangsufeng@googlemail.com>
2024-02-15 22:02:22 +0100

    add cvae model

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import numpy as np
 
 from sklearn.manifold import TSNE
-from CVAE import CVAE
+from VAE import VAE
 from NoisyDataGenerator import NoisyDataGenerator
 from plot import plot_encodings2d_with_labels
 from utils import get_mnist_data, sample_and_categorize, timer
 
 
 @timer
-def pretrain_cae(x_train, x_train_samples):
+def pretrain_cvae(x_train, x_train_samples):
     # x_train and x_train_samples are reshaped to 1-d vectors
 
     data_gen = NoisyDataGenerator(x_train, batch_size=128)
-    model_pre = CVAE()
+    model_pre = VAE()
 
     # %%time
     history = model_pre.model.fit(data_gen, epochs=10)
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     x_train_samples = np.reshape(samples, (-1, 784))
 
     # save model to local & import next time
-    clf, encodings_samples, encoder = pretrain_cae(x_train, x_train_samples)
+    clf, encodings_samples, encoder = pretrain_cvae(x_train, x_train_samples)
 
     # plot
     tsne = TSNE(n_components=2, random_state=42)
